29356c706ecb4e2ad2d6bc899f0266a46a3e6f97
Karolina Wolan <wolanka739@gmail.com>
2022-02-16 12:10:34 +0100

    corrected of mistakes

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@
           <button class="tasks__button tasks__button--toggleDone js-toggleDone">
             ${task.done ? "✔" : ""}
           </button>
-          <span class="tasks__content${task.done ? "tasks__content--done" : ""}">
+          <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
             ${task.content}
           </span>
           <button class="tasks__button tasks__button--remove js-remove">
